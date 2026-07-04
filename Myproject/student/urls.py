205fdf44dd3bcad7eaa6a26ba0ff9fa13b942d85
lithesh2006 <khlithesh57@gmail.com>
2026-07-04 08:21:08 +0530

    Delete Myproject directory

--- Myproject/student/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path,include
-from . import views
-urlpatterns = [
-
-    path('student-list/',views.Dispaly, name="student_list"),
-    
-]
--- Myproject/student/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def Dispaly(request):
-    return render(request,'student-list.html')
